6b6282fd298add1f0a76bee5d17199e1e63fbbb4
Kevin Bai <kevin.c.bai0@gmail.com>
2020-11-21 09:36:28 -0800

    Fix withDNAProps type definition

--- packages/design-system/src/helpers/withDNAProps.tsx
+++ packages/design-system/src/helpers/withDNAProps.tsx
@@ -3,10 +3,10 @@ import { DNA } from '../theme/types';
 import { extractDNAProps } from './extractDna';
 
 export function withDNAProps<T extends { dna: DNA }>(Comp: React.ComponentType<T>) {
-  return (props: T & DNA & { style?: object }) => {
+  return (props: Omit<T, 'dna'> & DNA & { style?: object }) => {
     const [dnaProps, otherProps] = extractDNAProps(props);
     return (
-      <Comp {...otherProps} dna={dnaProps} />
+      <Comp {...otherProps as any} dna={dnaProps} />
     )
   }
 }
